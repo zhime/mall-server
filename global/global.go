a6a79d26e53f7c4e06d7f1e8501bf44e6e817a9d
zhime <931013000@qq.com>
2025-03-11 15:48:33 +0800

    update

--- global/global.go
+++ global/global.go
@@ -3,9 +3,11 @@ package global
 import (
 	"github.com/spf13/viper"
 	"github.com/zhime/mall-server/config"
+	"go.uber.org/zap"
 )
 
 var (
 	MS_VP     *viper.Viper
 	MS_CONFIG *config.Server
+	MS_LOG    *zap.Logger
 )
